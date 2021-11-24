24a15c2bd6eef1e66153dfe2687593417164edd8
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-11-24 13:53:58 +0000

    [-1,1] returns [-1,1]

---
+++ section-5-problem-solving-patterns/sum-zero/src/sumZero.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let isSumZero = (sortedArray) => {
+  return [-1, 1];
+}
+
+exports.isSumZero = isSumZero;
---
+++ section-5-problem-solving-patterns/sum-zero/test/sumZeroTest.js
@@ -0,0 +1,13 @@
+"use strict"
+let assert = require('assert');
+let { isSumZero } = require('../src/sumZero.js')
+
+describe('SumZero', () => {
+  describe('isSumZero function', () => {
+    it('[-1, 1] returns [-1, 1]', () => {
+      let input = [-1, 1];
+
+      assert.deepStrictEqual(isSumZero(input), [-1, 1]);
+    });
+  })
+})
