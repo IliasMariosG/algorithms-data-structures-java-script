0b697d165cf9949cec9a7aa95bef26bd9c7ff52d
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-11-24 18:32:27 +0000

    It returns the correct result when the right side of the array has a larger number

--- section-5-problem-solving-patterns/sum-zero/src/sumZero.js
+++ section-5-problem-solving-patterns/sum-zero/src/sumZero.js
@@ -1,8 +1,19 @@
 "use strict"
 
 let isSumZero = (sortedArray) => {
-  if (sortedArray[0] + sortedArray[sortedArray.length-1] === 0) {
-    return [sortedArray[0], sortedArray[sortedArray.length-1]];
+  let i = 0;
+  let j = sortedArray.length-1;
+  for (i; i < j;) {
+    if ((sortedArray[i] === 0) || (sortedArray[j] === 0)) {
+      continue;
+    };
+    if (sortedArray[i] + sortedArray[j] === 0) {
+      return [sortedArray[i], sortedArray[j]];
+    } else if (sortedArray[i] + sortedArray[j] > 0) {
+      j--
+    } else {
+      i++
+    };
   }
   return undefined
 }
--- section-5-problem-solving-patterns/sum-zero/test/sumZeroTest.js
+++ section-5-problem-solving-patterns/sum-zero/test/sumZeroTest.js
@@ -4,30 +4,51 @@ let { isSumZero } = require('../src/sumZero.js')
 
 describe('SumZero', () => {
   describe('isSumZero function', () => {
+    let input;
     it('[-1, 1] returns [-1, 1]', () => {
-      let input = [-1, 1];
+      input = [-1, 1];
 
       assert.deepStrictEqual(isSumZero(input), [-1, 1]);
     });
     it('[-2, 2] returns [-2, 2]', () => {
-      let input = [-2, 2];
+      input = [-2, 2];
 
       assert.deepStrictEqual(isSumZero(input), [-2, 2]);
     });
     it('[-50, 1, 50] returns [-50, 50]', () => {
-      let input = [-50, 1, 50];
+      input = [-50, 1, 50];
 
       assert.deepStrictEqual(isSumZero(input), [-50, 50]);
     });
     it('[-50, 35, 47, 50] returns [-50, 50]', () => {
-      let input = [-50, 35, 39, 47, 50];
+      input = [-50, 35, 39, 47, 50];
 
       assert.deepStrictEqual(isSumZero(input), [-50, 50]);
     });
     it('[-3, 1] returns undefined', () => {
-      let input = [-3, 1];
+      input = [-3, 1];
 
       assert.deepStrictEqual(isSumZero(input), undefined);
     });
+    it('[-3, -1, 1] returns [-1, 1]', () => {
+      input = [-3, -1, 1];
+
+      assert.deepStrictEqual(isSumZero(input), [-1, 1]);
+    });
+    it('[-3, -2, 0, 1, 2, 4] returns [-2, 2]', () => {
+      input = [-3, -2, 0, 1, 2, 4];
+
+      assert.deepStrictEqual(isSumZero(input), [-2, 2]);
+    });
+    it('[0] returns undefined', () => {
+      input = [0];
+
+      assert.deepStrictEqual(isSumZero(input), undefined)
+    })
+    it('[-2, 0, 1, 3] returns undefined', () => {
+      input = [-2, 0, 1, 3];
+
+      assert.deepStrictEqual(isSumZero(input), undefined)
+    })
   })
 })
