62ad30a4daac0cb36d3f7adfa376b2491b254e22
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-11-30 14:49:00 +0000

    Return correct sum for consecutive elements of array

---
+++ section-5-problem-solving-patterns/sliding-window-pattern/max-sum-refactored/src/maxSumRefactored.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const MaxSum = class {
+  findMaxSum(numbers, count) {
+    if (numbers.length === 0) {
+      return null;
+    }
+    let maximumSum = 0;
+  //   // Keep track of the temporary sum of n consecutive elements
+    let temporarySum = 0;
+    for (let i = 0; i < count; i++) {
+      // set maximum sum to the first 'count'=2 consecutive elements
+      maximumSum += numbers[i];
+    }
+    temporarySum = maximumSum;
+    for (let i = count; i < numbers.length; i++) {
+  //     // for i == 0:
+  //     // it substracts from maximumSum (temporarySum==maximumSum)
+  //     // it adds the right element on index i
+  //     // it substracts the left element on index i-2
+  //     // for i == 1:
+  //     // ..
+  //     // ..
+  //     // 
+      temporarySum = temporarySum + numbers[i] - numbers[i-count]
+      maximumSum = Math.max(maximumSum, temporarySum)
+    }
+    return maximumSum
+  }
+}
+
+exports.MaxSum = MaxSum;
---
+++ section-5-problem-solving-patterns/sliding-window-pattern/max-sum-refactored/test/maxSumRefactored.test.js
@@ -0,0 +1,26 @@
+'use strict';
+const assert = require('assert');
+let { MaxSum } = require('../src/maxSumRefactored.js')
+
+describe('Max Sum Refactored Class', () => {
+  let maxSumInstance;
+  let window;
+  beforeEach('class initialisation', function() {
+    maxSumInstance = new MaxSum();
+  });
+  describe('#findMaxSum', () => {
+    it('[1,2,3] with window 2 returns 5', () => {
+      maxSumInstance = new MaxSum();
+      window = 2;
+
+      assert.deepStrictEqual(maxSumInstance.findMaxSum([1, 2, 3], window), 5)
+    });
+    it('[2,6,9,2,1,8,5,6,3] with window 3 returns 19', () => {
+      maxSumInstance = new MaxSum();
+      window = 3;
+
+      assert.deepStrictEqual(maxSumInstance.findMaxSum([2,6,9,2,1,8,5,6,3], window), 19)
+    });
+    
+  })
+})
